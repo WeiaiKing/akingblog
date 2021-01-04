1295a320aaa3c97f9bbbffb7e86c11b15445dc81
韦加金 <1192620781@qq.com>
2021-01-04 09:44:31 +0800

    feat: 评论接口

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,7 +3,7 @@ const connection = require('../app/database');
 class UserService {
   async create(user) {
     const { name, password } = user;
-    const statement = `INSERT INTO users (name, password) VALUES (?, ?);`;
+    const statement = `INSERT INTO user (name, password) VALUES (?, ?);`;
     const result = await connection.execute(statement, [name, password]);
     return result[0];
   }
